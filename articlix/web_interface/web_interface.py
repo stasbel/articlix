477533f92622b78d5fb8ee777dcfd021326d0bcc
noscode <katenos823@gmail.com>
2017-12-13 11:50:19 +0000

    For 0

--- articlix/web_interface/web_interface.py
+++ articlix/web_interface/web_interface.py
@@ -233,7 +233,7 @@ class Interface:
     def run(self):
         try:
             from wsgiref.simple_server import make_server
-            httpd = make_server('', 8080, self.refresh)
+            httpd = make_server('0.0.0.0', 5000, self.refresh)
             print('Serving on http://127.0.0.1:8080...')
             httpd.serve_forever()
         except KeyboardInterrupt:
